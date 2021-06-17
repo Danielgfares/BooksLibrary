e5fe029356baa225676f243518406d3ce86f3885
Danielgfares <danifaress@hotmail.com>
2021-06-17 02:26:04 +0200

    Add project files.

---
+++ BooksLibrary.cpp
@@ -0,0 +1,25 @@
+// BooksLibrary.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+/**
+Design a set of types that represent the items that can be borrowed at a library. 
+This library offers books, CDs, DVDs and Blue-ray discs, and some titles can be downloaded into a file as 
+eBooks, audio or video files. 
+All kinds of items share a title and an ISBN. Books have an author and contain several pages, whereas CDs, 
+DVDs and Blue-ray discs can hold multiple tracks of audio or video each having a separate title and artist 
+as well as a duration. 
+Your task is to design a set of types that can be used to retrieve the information mentioned above 
+representing these various kinds of items. Please use constructs such as inheritance, interfaces, and 
+virtual functions as you deem appropriate and focus on public members defining the accessible surface 
+of each type. You do not need to actually code the implementation of any methods and you also do not 
+need to deal with the different file formats for downloading. Diagrams are appreciated.
+*/
